364711dfc9f47776675c0c1880a786d221f41725
gooodboyAo <ao1518782@163.com>
2022-09-29 16:13:31 +0800

    --add app01

---
+++ JenkinsDjango/test.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+"""
+@python_version: python3.7.2
+@author: ran.ao
+@contact: ran.ao@daocloud.io
+@software: PyCharm
+@file: test.py
+@time: 2022/9/29 -16:11
+@file_version:
+@describe:
+
+"""
+print(123)
